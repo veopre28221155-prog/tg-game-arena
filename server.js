b3e83375a6bb612e02f7b70b5b364aed6b925281
veopre28221155-prog <veopre28221155@uamx.purpleanchora.com>
2026-02-28 22:21:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-
 const app = express();
-app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-
-// Просто раздаем файлы
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +9,5 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`🚀 Сервер запущен`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Neural Arena запущен на порту ${PORT}`));
